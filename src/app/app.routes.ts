72a867a249f918df02384c55cb8a8a9b69cf7db0
brettblomquist <brett.blomquist@ndsu.edu>
2025-04-25 09:27:51 -0500

    Added sign-up page and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: SignInComponent, title: 'Sign In'},
+    {path:'sign-in', component: SignInComponent, title: 'Sign In'
+    },
+    {path:'sign-up-page', component: SignUpPageComponent, title: 'Sign Up'},
+
+];
