6cb5818fb977b19ba98175cdd9c712f72688fd71
shuang1219 <shuang.wu@ndsu.edu>
2025-04-26 18:16:12 -0500

    Add teacher dashboard page with layout, animation and styling

--- just-due-it/src/app/app.routes.ts
+++ just-due-it/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { TeacherDashboardComponent } from './pages/teacher-dashboard/teacher-dashboard.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: 'teacher-dashboard',
+    component: TeacherDashboardComponent
+  }
+];
