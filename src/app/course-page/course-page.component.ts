e4758481b641c70ee57ec57fad9a154355d58bf9
Elaina-Haugen <elaina.haugen@ndsu.edu>
2025-05-08 22:07:29 -0500

    sidebar and grade calc

--- src/app/course-page/course-page.component.ts
+++ src/app/course-page/course-page.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Firestore, collection, collectionData, doc, docData } from '@angular/fire/firestore';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-course-page',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './course-page.component.html',
   styleUrl: './course-page.component.css'
 })
@@ -27,5 +29,25 @@ export class CoursePageComponent {
       collectionData(assignmentsCollection, {idField: 'id'}).subscribe((assignments) => {
         this.assignments = assignments;
       })
+    }
+
+    weighted = [0, 0, 0, 0, 0];
+    earned = [0, 0, 0, 0, 0];
+    total= [0, 0, 0, 0 ,0];
+
+    finalGrade: number | null= null;
+
+    calculateGrade() {
+      let weightedSum = 0;
+      let totalWeight = 0;
+  
+      for (let i = 0; i < 5; i++) {
+        const weight = this.weighted[i] / 100;
+        const score = this.total[i] ? this.earned[i] / this.total[i] : 0;
+        weightedSum += score * weight;
+        totalWeight += weight;
+      }
+  
+      this.finalGrade = totalWeight > 0 ? +(weightedSum / totalWeight * 100).toFixed(2) : null;
     }
 }
